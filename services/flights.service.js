54b161ebc01e3d11673f3a3151b4789738b944bc
Rob <rwrobe@gmail.com>
2017-11-07 22:24:04 -0500

    Changed PnP package, revised the polygon boundaries and logic for checking whether we're in or out of them. Still no hits

--- services/flights.service.js
+++ services/flights.service.js
@@ -1,6 +1,6 @@
 "use strict";
-const Request = require( 'request' );
-const PnP = require( 'robust-point-in-polygon' );
+const Request = require( "request" );
+const PnP = require( "point-in-polygon" );
 
 module.exports = {
 	name: "flights",
@@ -21,17 +21,57 @@ module.exports = {
 		 * @returns
 		 */
 		all_us() {
-			let url = 'https://opensky-network.org/api/states/all';
+			let url = "https://opensky-network.org/api/states/all";
+			let polygon = [
+				[
+					38.895643,
+					- 77.072973
+				],
+				[
+					38.912033,
+					- 77.060248
+				],
+				[
+					38.905492,
+					- 77.028271
+				],
+				[
+					38.883298,
+					- 77.006018
+				],
+				[
+					38.850398,
+					- 77.017953
+				],
+				[
+					38.845780,
+					- 77.056642
+				]
+			];
 			let flights = Request( url, function ( error, response, body ) {
 
-				let rsp = JSON.parse( body ); // Print the HTML for the Google homepage.
+				let rsp = JSON.parse( body );
+				let pointInPoly = false;
+				let matchingFlights = rsp;
 
-				for ( let i = 0; i < rsp['states'].length; i ++ ) {
-					if ( 'United States' === rsp['states'][i][2] ) {
-						console.log( JSON.stringify( rsp['states'][i][0] ) );
+				for ( let i = 0; i < rsp["states"].length; i ++ ) {
+					if ( "United States" !== rsp["states"][i][2] ) {
+						continue;
+					}
+
+					pointInPoly = PnP( [rsp["states"][i][6], rsp["states"][i][5]], polygon );
+
+					if ( true === pointInPoly ) {
+						console.log( [rsp["states"][i][6], rsp["states"][i][5]] );
+
+						matchingFlights[i] = rsp["states"][i][0];
 					}
 				}
+
+				return matchingFlights;
 			} );
+
+			return flights;
 		},
 
 		/**
@@ -45,7 +85,7 @@ module.exports = {
 			},
 			handler( ctx ) {
 
-				let url = 'https://ae.roplan.es/api/hex-reg.php?hex=';
+				let url = "https://ae.roplan.es/api/hex-reg.php?hex=";
 				let response = '';
 				let registration = Request( url + ctx.params.hex, function ( error, response, body ) {
 					console.log( body );
